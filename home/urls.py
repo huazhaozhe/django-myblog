0de8344281288dd634a48f7e8b255f0f88aa0b5e
zhe <1655604975@qq.com>
2018-07-29 15:27:43 +0800

    修改首页缓存为模板缓存

--- home/urls.py
+++ home/urls.py
@@ -3,9 +3,9 @@
 
 from django.conf.urls import url
 from . import views
-from django.views.decorators.cache import cache_page
+
 
 urlpatterns = [
-    url(r'^$', cache_page(7 * 24 * 60 * 60)(views.home), name='home'),
-    url(r'^about/$', cache_page(7 * 24 * 60 * 60)(views.about), name='about'),
+    url(r'^$', views.home, name='home'),
+    url(r'^about/$', views.about, name='about'),
 ]
