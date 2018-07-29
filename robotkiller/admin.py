c07f725935db6eea427a7d5ddca77a2360e905de
zhe <1655604975@qq.com>
2018-07-29 15:44:30 +0800

    添加addrkiller至admin

--- robotkiller/admin.py
+++ robotkiller/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from robotkiller.models import AddrKiller
+
+class AddrKillerAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(AddrKiller, AddrKillerAdmin)
